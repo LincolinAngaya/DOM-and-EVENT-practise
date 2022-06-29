dc36bec713b675ce543db0691b85aeb2b99826f5
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-29 14:25:45 +0300

    dom practise

--- main.js
+++ main.js
@@ -21,3 +21,25 @@ function onsubmit(e) {
     nameInput.value = '';
     emailInput.value = '';
 }
+//add newelement in a dom
+var para = document.createElement('p');
+
+// para.className = "paragraph"
+// para.id = "paragraph1"
+// para.setAttribute('title', 'this is my paragraph')
+
+// var headingText = document.createTextNode('this is my first paragragh')
+// para.appendChild(headingText)
+// console.log(headingText)
+// var body = document.querySelector('body')
+// var container = document.querySelector('container')
+// body.insertBefore(para, container)
+
+//replacing child
+var head = document.createElement('h2')
+var node = document.createTextNode('javascript for denior')
+head.appendChild(node)
+
+var body = document.querySelector('header')
+var h1 = document.querySelector('h1')
+body.replaceChild(head, h1);
